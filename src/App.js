7cd0286774a09bda9304c21309228fc568189fba
Augusto Corvalan <augustocorvalan@gmail.com>
2021-03-23 21:31:40 -0700

    Add Generation screen

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import InputScreen from "./InputScreen"
+import GenerationScreen from "./GenerationScreen"
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+        <GenerationScreen />
         <InputScreen />
     </div>
   );
---
+++ src/GenerationScreen.js
@@ -0,0 +1,10 @@
+
+const GenerationScreen = () => {
+    return (
+        <div>
+            <h2>Generation Screen</h2>
+        </div>
+    )
+}
+
+export default GenerationScreen 
