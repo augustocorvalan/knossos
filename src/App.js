d9f72e9baf1865d6e582cb0ab28c5a611c40d56c
Augusto Corvalan <augustocorvalan@gmail.com>
2021-03-24 15:58:36 -0700

    Hook up input screen to markov model logic

--- src/App.js
+++ src/App.js
@@ -4,16 +4,50 @@ import InputScreen from "./InputScreen"
 import GenerationScreen from "./GenerationScreen"
 import logo from './logo.svg';
 import './App.css';
+import MarkovGenerator from "./utils/MarkovGenerator"
+
 
 const SCREENS = {
   INPUT: "INPUT",
   GENERATION: "GENERATION"
 }
+
+function getModel(input) {
+  const model = new MarkovGenerator(); // Create a Markov chain
+
+  // Teach the chain some text.
+  model.feed(input);
+
+  return model
+}
+
 function App() {
   const [currentScreen, setCurrentScreen] = useState(SCREENS.INPUT)
+  const [currentInput, setCurrentInput] = useState("")
+  const [generationModel, setGenerationModel] = useState(null)
+
+  /**
+   * @private handleInputChange
+   */
+  const handleInputChange = (event) => {
+    const value = event.target.value 
+
+    setCurrentInput(value)
+  }
+  /**
+   * @private handleInputChange
+   */
+  const handleInputSubmit = () => {
+    console.log("!!! current input", currentInput)
+    /* create a new model out of the current user input */
+    const model = getModel(currentInput)
+    setGenerationModel(model)
+    /* proceed to generation page */
+    setCurrentScreen(SCREENS.GENERATION)
+  }
   return (
     <div className="App">
-      {currentScreen === SCREENS.INPUT && <InputScreen /> }
+      {currentScreen === SCREENS.INPUT && <InputScreen onInputChange={handleInputChange} onInputSubmit={handleInputSubmit} /> }
       {currentScreen === SCREENS.GENERATION && <GenerationScreen /> }
     </div>
   );
